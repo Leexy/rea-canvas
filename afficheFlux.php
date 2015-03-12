8bace737bbd6deb91ca34397d469ea7e6552c37c
Leexy <agathe.grunberg@gmail.com>
2015-03-12 18:16:36 +0100

    affichage flux categorie + random

--- afficheFlux.php
+++ afficheFlux.php
@@ -1,78 +1,49 @@
 <?php
 
 /* tableau de flux */
-$arrayFlux = array();
-
-/* verifie si le bouton est clique et definit $url */
-if (isset($_POST['Category']) && $_POST['Category'] =="Politique"){
-	$arrayFlux[] = array(
-	    "category" => "Politique",
+$tabFlux = array(
+ array(
+		"category" => "Politique",
 	    "url" => "http://www.lemonde.fr/politique/rss_full.xml",
-	    "color"   =>"red",
-	);
-}
-if (isset($_POST['Category']) && $_POST['Category'] == "Culture"){
-	$arrayFlux[] = array(
-	    "category" => "Culture",
-	    "url" => "http://www.france24.com/fr/culture/rss/",
-	    "color"   =>"blue",
-	);
-}
-if (isset($_POST['Category']) && $_POST['Category'] == "People"){
-	$arrayFlux[] = array(
-	    "category" => "People",
-	    "url" => "http://www.purepeople.com/rss/news_t0.xml",
-	    "color"   =>"green",
-	);
-}
-if (isset($_POST['Category']) && $_POST['Category'] == "Sport"){
-	$arrayFlux[] = array(
-	    "category" => "Sport",
-	    "url" => "http://www.eurosport.fr/rss.xml",
-	    "color"   =>"fuchsia",
-	);
-}
-if (isset($_POST['Category']) && $_POST['Category'] == "International"){
-	$arrayFlux[] = array(
-	    "category" => "International",
-	    "url" => "http://www.lemonde.fr/international/rss_full.xml",
-	    "color"   =>"LightSlateGray",
-	);
-}
-if (isset($_POST['Category']) && $_POST['Category'] == "Sciences"){
-  $arrayFlux[] = array(
-	    "category" => "Sciences",
-	    "url" => "http://www.sciencesetavenir.fr/rss.xml",
-	    "color"   =>"cyan",
-	);
-}
-if (isset($_POST['Category']) && $_POST['Category'] == "Cinema"){
-	$arrayFlux[] = array(
-	    "category" => "Cinema",
-	    "url" => "http://www.premiere.fr/var/premiere/storage/rss/cinema_actu.xml",
-	    "color"   =>"LightSeaGreen",
-	);
-} 
-if (isset($_POST['Category']) && $_POST['Category'] == "Technologie"){
-	$arrayFlux[] = array(
-	    "category" => "Technologie",
-	    "url" => "http://www.actinnovation.com/feed/",
-	    "color"   =>"orange",
-	);
-}
-if (isset($_POST['Category']) && $_POST['Category'] == "Economie"){
-	$arrayFlux[] = array(
+	),
+ array(
 	    "category" => "Economie",
 	    "url" => "http://syndication.lesechos.fr/rss/rss_politique_societe.xml",
-	    "color"   =>"Yellow",
-	);
-}
+	),
+ array(
+	    "category" => "Technologie",
+	    "url" => "http://www.actinnovation.com/feed/",
+	),
+ array(
+	    "category" => "Cinema",
+	    "url" => "http://www.premiere.fr/var/premiere/storage/rss/cinema_actu.xml",
+	),
+ array(
+	    "category" => "Sciences",
+	    "url" => "http://www.sciencesetavenir.fr/rss.xml",
+	),
+ array(
+	    "category" => "International",
+	    "url" => "http://www.lemonde.fr/international/rss_full.xml",
+	),
+ array(
+	    "category" => "Sport",
+	    "url" => "http://www.eurosport.fr/rss.xml",
+	),
+ array(
+	    "category" => "People",
+	    "url" => "http://www.purepeople.com/rss/news_t0.xml",
+	),
+array(
+	    "category" => "Culture",
+	    "url" => "http://www.france24.com/fr/culture/rss/",
+	)
+);
 
-/* affichage des flux */
+/* affichage des flux de la categorie choisie */
 $arrayItems = array();
-foreach ($arrayFlux as $array){
-	if(isset($array['url'])){
-
+foreach ($tabFlux as $key => $array){
+	if(isset($array['url']) && $_POST['Category'] == $array['category'] ){
 		$rss = simplexml_load_file($array['url']);
 		if($rss)
 		{
@@ -87,9 +58,30 @@ foreach ($arrayFlux as $array){
  					'category' => (string) $array['category']
 				);
 			}
+			unset($tabFlux[$key]); // supprime le flux selectionne du tableau pour qu'il ne reapparaissent pas dans les flux random !
 		}
 	}
 }
+/* affichage de flux de categories random */
+$rand_keys = shuffle($tabFlux);
+foreach(array_slice($tabFlux, 0, 3) as $key => $array) {
+	$rss = simplexml_load_file($array['url']);
+	if($rss)
+	{
+		$items = $rss->channel->item;
+		foreach($items->xpath('//item[position() <= 2]') as $item) // on limite le nombre de flux a 4 par categorie
+		{
+			$published_on = $item->pubDate;
+			$pubDate = strftime("%d-%m-%Y %H:%M", strtotime($published_on));
+			$arrayItems[] = array(
+				'title' => (string) $item->title,
+					'pubdate' => (string) $pubDate,
+					'category' => (string) $array['category']
+			);
+		}
+	}
+}
+
 echo json_encode($arrayItems);
 
 ?>
