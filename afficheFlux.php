6d41a81c681aaa906b002ce6dfa19ff48d38bd58
Leexy <agathe.grunberg@gmail.com>
2015-03-13 18:09:29 +0100

    ajout des fonts par catégorie

--- afficheFlux.php
+++ afficheFlux.php
@@ -2,24 +2,25 @@
 
 /* tableau de flux */
 $tabFlux = array(
-		"Politique" => array("url" => "http://www.lemonde.fr/politique/rss_full.xml", "categorie" => "Politique"),
+		"Politique" => array("url" => "http://www.lemonde.fr/politique/rss_full.xml", "categorie" => "Politique", "font" => "20px Times"),
 
-	    "Economie" => array("url" => "http://syndication.lesechos.fr/rss/rss_politique_societe.xml", "categorie" => "Economie"),
+	    "Economie" => array("url" => "http://syndication.lesechos.fr/rss/rss_politique_societe.xml", "categorie" => "Economie", "font" => "Italic 20px Palatino"),
 
-	    "Technologie" => array("url" => "http://www.actinnovation.com/feed/", "categorie" =>  "Technologie"),
+	    "Technologie" => array("url" => "http://www.actinnovation.com/feed/", "categorie" =>  "Technologie", "font" => "20px Gill"),
 
-	    "Cinema" => array("url" => "http://www.premiere.fr/var/premiere/storage/rss/cinema_actu.xml", "categorie" => "Cinema"),
+	    "Cinema" => array("url" => "http://www.premiere.fr/var/premiere/storage/rss/cinema_actu.xml", "categorie" => "Cinema", "font" => "20px Andale mono"),
 
-	    "Sciences" => array("url" => "http://www.sciencesetavenir.fr/rss.xml", "categorie" => "Sciences"),
+	    "Sciences" => array("url" => "http://www.sciencesetavenir.fr/rss.xml", "categorie" => "Sciences", "font" => "20px Courrier"),
 
-	    "International" => array("url" => "http://www.lemonde.fr/international/rss_full.xml", "categorie" => "International"),
+	    "International" => array("url" => "http://www.lemonde.fr/international/rss_full.xml", "categorie" => "International", "font" => "Bold 20px Helvetica Narrow"),
 
-	    "Sport" => array("url" => "http://www.eurosport.fr/rss.xml", "categorie" => "Sport"),
+	    "Sport" => array("url" => "http://www.eurosport.fr/rss.xml", "categorie" => "Sport", "font" => "20px Impact"),
 
-	    "People" => array("url" => "http://www.purepeople.com/rss/news_t0.xml", "categorie" => "People"),
+	    "People" => array("url" => "http://www.purepeople.com/rss/news_t0.xml", "categorie" => "People", "font" => "20px Arial"),
 
-	    "Culture" => array("url" => "http://www.france24.com/fr/culture/rss/", "categorie" => "Culture"),
+	    "Culture" => array("url" => "http://www.france24.com/fr/culture/rss/", "categorie" => "Culture", "font" => "20px Parkavenue"),
 );
+
 
 /* affichage des flux de la categorie choisie */
 $arrayItems = array();
@@ -36,7 +37,8 @@ if(isset($_POST['Category'])){
 				$arrayItems[] = array(
 					'title' => (string) $item->title,
  					'pubdate' => (string) $pubDate,
- 					'category' => (string) $cat
+ 					'category' => (string) $cat,
+ 					'font' => (string) $tabFlux[$cat]['font']
 				);
 			}
 			unset($tabFlux[$cat]); // supprime le flux selectionne du tableau pour qu'il ne reapparaissent pas dans les flux random !
@@ -57,7 +59,8 @@ foreach(array_slice($tabFlux, 0, 3, true) as $category => $flux) {
 			$arrayItems[] = array(
 				'title' => (string) $item->title,
 				'pubdate' => (string) $pubDate,
-				'category' => (string) $flux['categorie']
+				'category' => (string) $flux['categorie'],
+ 				'font' => (string) $flux['font']
 			);
 		}
 	}
