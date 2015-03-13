be2c06963d72c569dffdc5674229a50da5560c11
Leexy <agathe.grunberg@gmail.com>
2015-03-13 20:30:44 +0100

    img & css page accueil

--- index.php
+++ index.php
@@ -6,29 +6,31 @@
   <link rel="stylesheet" href="css/style.css" type="text/css" />
 </head>
 
-<body>
+<body class="index-page">
+<img class="titre" src="img/titre.png"/>
+<img class="titre" src="img/soustitre.png"/>
 <div class="table">
   <div class="row">
-  	<div class="categorie" id="Politique">Politique</div>
-  	<div class="categorie" id="Culture">Culture</div>
-    <div class="categorie" id="People">People</div>
+  	<div class="categorie" id="Politique" value="Politique"></div>
+  	<div class="categorie" id="Culture" value="Culture"></div>
+    <div class="categorie" id="People" value="People"></div>
   </div>
   <div class="row">
-    <div class="categorie" id="Sport">Sport</div>
-    <div class="categorie" id="International">International</div>
-    <div class="categorie" id="Sciences">Sciences</div><br/>
+    <div class="categorie" id="Sport" value="Sport"></div>
+    <div class="categorie" id="International" value="International"></div>
+    <div class="categorie" id="Sciences" value="Sciences"></div>
   </div>
   <div class="row">
-    <div class="categorie" id="Cinema">Cinema</div>
-    <div class="categorie" id="Technologie">Technologie</div>
-    <div class="categorie" id="Economie">Economie</div><br/>
+    <div class="categorie" id="Cinema" value="Cinema"></div>
+    <div class="categorie" id="Technologie" value="Technologie"></div>
+    <div class="categorie" id="Economie" value="Economie"></div>
   </div>
 </div>
-
+<div class="letter-img"></div>
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script type="text/javascript">
 $("div.categorie").click(function() {
-  var cat = $(this).text();
+  var cat = $(this).attr('value');
   document.location = "game.php?categorie="+encodeURIComponent(cat);
   });
 </script>
