17e2ee365e217bd869efec7fba9495e098e169ca
Leexy <agathe.grunberg@gmail.com>
2015-03-13 22:13:17 +0100

    update font size

--- afficheFlux.php
+++ afficheFlux.php
@@ -2,23 +2,23 @@
 
 /* tableau de flux */
 $tabFlux = array(
-		"Politique" => array("url" => "http://www.lemonde.fr/politique/rss_full.xml", "categorie" => "Politique", "font" => "20px Times"),
+		"Politique" => array("url" => "http://www.lemonde.fr/politique/rss_full.xml", "categorie" => "Politique", "font" => "1.2em Times"),
 
-	    "Economie" => array("url" => "http://syndication.lesechos.fr/rss/rss_politique_societe.xml", "categorie" => "Economie", "font" => "Italic 20px Palatino"),
+	    "Economie" => array("url" => "http://syndication.lesechos.fr/rss/rss_politique_societe.xml", "categorie" => "Economie", "font" => "Italic 1.2em Palatino"),
 
-	    "Technologie" => array("url" => "http://www.actinnovation.com/feed/", "categorie" =>  "Technologie", "font" => "20px Gill"),
+	    "Technologie" => array("url" => "http://www.actinnovation.com/feed/", "categorie" =>  "Technologie", "font" => "Bold 0.8em Gill Sans"),
 
-	    "Cinema" => array("url" => "http://www.premiere.fr/var/premiere/storage/rss/cinema_actu.xml", "categorie" => "Cinema", "font" => "20px Andale mono"),
+	    "Cinema" => array("url" => "http://www.premiere.fr/var/premiere/storage/rss/cinema_actu.xml", "categorie" => "Cinema", "font" => "1.8em Andale Mono"),
 
-	    "Sciences" => array("url" => "http://www.sciencesetavenir.fr/rss.xml", "categorie" => "Sciences", "font" => "20px Courrier"),
+	    "Sciences" => array("url" => "http://www.sciencesetavenir.fr/rss.xml", "categorie" => "Sciences", "font" => "1em Courrier"),
 
-	    "International" => array("url" => "http://www.lemonde.fr/international/rss_full.xml", "categorie" => "International", "font" => "Bold 20px Helvetica Narrow"),
+	    "International" => array("url" => "http://www.lemonde.fr/international/rss_full.xml", "categorie" => "International", "font" => "Bold 1.4em Helvetica Narrow"),
 
-	    "Sport" => array("url" => "http://www.eurosport.fr/rss.xml", "categorie" => "Sport", "font" => "20px Impact"),
+	    "Sport" => array("url" => "http://www.eurosport.fr/rss.xml", "categorie" => "Sport", "font" => "1.2em Impact"),
 
-	    "People" => array("url" => "http://www.purepeople.com/rss/news_t0.xml", "categorie" => "People", "font" => "20px Arial"),
+	    "People" => array("url" => "http://www.purepeople.com/rss/news_t0.xml", "categorie" => "People", "font" => "1.2em Arial"),
 
-	    "Culture" => array("url" => "http://www.france24.com/fr/culture/rss/", "categorie" => "Culture", "font" => "20px Parkavenue"),
+	    "Culture" => array("url" => "http://www.france24.com/fr/culture/rss/", "categorie" => "Culture", "font" => "1em Parkavenue"),
 );
 
 
