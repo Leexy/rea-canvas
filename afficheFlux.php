b597831a222c80578cc360b8e3ad9b0894ef1d1b
Leexy <agathe.grunberg@gmail.com>
2015-03-13 09:09:24 +0100

    changement structuration de tabFlux

--- afficheFlux.php
+++ afficheFlux.php
@@ -2,49 +2,30 @@
 
 /* tableau de flux */
 $tabFlux = array(
- array(
-		"category" => "Politique",
-	    "url" => "http://www.lemonde.fr/politique/rss_full.xml",
-	),
- array(
-	    "category" => "Economie",
-	    "url" => "http://syndication.lesechos.fr/rss/rss_politique_societe.xml",
-	),
- array(
-	    "category" => "Technologie",
-	    "url" => "http://www.actinnovation.com/feed/",
-	),
- array(
-	    "category" => "Cinema",
-	    "url" => "http://www.premiere.fr/var/premiere/storage/rss/cinema_actu.xml",
-	),
- array(
-	    "category" => "Sciences",
-	    "url" => "http://www.sciencesetavenir.fr/rss.xml",
-	),
- array(
-	    "category" => "International",
-	    "url" => "http://www.lemonde.fr/international/rss_full.xml",
-	),
- array(
-	    "category" => "Sport",
-	    "url" => "http://www.eurosport.fr/rss.xml",
-	),
- array(
-	    "category" => "People",
-	    "url" => "http://www.purepeople.com/rss/news_t0.xml",
-	),
-array(
-	    "category" => "Culture",
-	    "url" => "http://www.france24.com/fr/culture/rss/",
-	)
+		"Politique" => array("url" => "http://www.lemonde.fr/politique/rss_full.xml", "categorie" => "Politique"),
+
+	    "Economie" => array("url" => "http://syndication.lesechos.fr/rss/rss_politique_societe.xml", "categorie" => "Economie"),
+
+	    "Technologie" => array("url" => "http://www.actinnovation.com/feed/", "categorie" =>  "Technologie"),
+
+	    "Cinema" => array("url" => "http://www.premiere.fr/var/premiere/storage/rss/cinema_actu.xml", "categorie" => "Cinema"),
+
+	    "Sciences" => array("url" => "http://www.sciencesetavenir.fr/rss.xml", "categorie" => "Sciences"),
+
+	    "International" => array("url" => "http://www.lemonde.fr/international/rss_full.xml", "categorie" => "International"),
+
+	    "Sport" => array("url" => "http://www.eurosport.fr/rss.xml", "categorie" => "Sport"),
+
+	    "People" => array("url" => "http://www.purepeople.com/rss/news_t0.xml", "categorie" => "People"),
+
+	    "Culture" => array("url" => "http://www.france24.com/fr/culture/rss/", "categorie" => "Culture"),
 );
 
 /* affichage des flux de la categorie choisie */
 $arrayItems = array();
-foreach ($tabFlux as $key => $array){
-	if(isset($array['url']) && $_POST['Category'] == $array['category'] ){
-		$rss = simplexml_load_file($array['url']);
+if(isset($_POST['Category'])){
+	$cat = $_POST['Category'];
+		$rss = simplexml_load_file($tabFlux[$cat]['url']);
 		if($rss)
 		{
 			$items = $rss->channel->item;
@@ -55,17 +36,17 @@ foreach ($tabFlux as $key => $array){
 				$arrayItems[] = array(
 					'title' => (string) $item->title,
  					'pubdate' => (string) $pubDate,
- 					'category' => (string) $array['category']
+ 					'category' => (string) $cat
 				);
 			}
-			unset($tabFlux[$key]); // supprime le flux selectionne du tableau pour qu'il ne reapparaissent pas dans les flux random !
+			unset($tabFlux[$cat]); // supprime le flux selectionne du tableau pour qu'il ne reapparaissent pas dans les flux random !
 		}
-	}
 }
+
 /* affichage de flux de categories random */
 shuffle($tabFlux);
-foreach(array_slice($tabFlux, 0, 3) as $key => $array) {
-	$rss = simplexml_load_file($array['url']);
+foreach(array_slice($tabFlux, 0, 3, true) as $category => $flux) {
+	$rss = simplexml_load_file($flux['url']);
 	if($rss)
 	{
 		$items = $rss->channel->item;
@@ -76,7 +57,7 @@ foreach(array_slice($tabFlux, 0, 3) as $key => $array) {
 			$arrayItems[] = array(
 				'title' => (string) $item->title,
 				'pubdate' => (string) $pubDate,
-				'category' => (string) $array['category']
+				'category' => (string) $flux['categorie']
 			);
 		}
 	}
